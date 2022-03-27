a59714d910307a400fbf317aeb4dc6035723cd9b
denjasan <denjasan@gmail.com>
2022-03-27 19:57:30 +0300

    Format of code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 
-int main() {
-	std::string name; //переменная для имени
-	std::getline(std::cin, name); // читаем строку
-	std::cout << "Hello World from " << name << std::endl; //выводим результат 
-	return 0;
+int
+main()
+{
+  std::string name;             //переменная для имени
+  std::getline(std::cin, name); // читаем строку
+  std::cout << "Hello World from " << name << std::endl; //выводим результат
+  return 0;
 }
-
