55c0c552dfc3f1119f43ec666460f170a1dc6a5c
denjasan <denjasan@gmail.com>
2022-03-27 19:21:46 +0300

    Comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 int main() {
-	std::string name;
-	std::getline(std::cin, name);
-	std::cout << "Hello World from " << name << std::endl;
+	std::string name; //переменная для имени
+	std::getline(std::cin, name); // читаем строку
+	std::cout << "Hello World from " << name << std::endl; //выводим результат 
 	return 0;
 }
 
