29a2d84865b69777f957b9215957606488c020d7
Denis Rodionov <55495317+denjasan@users.noreply.github.com>
2022-03-29 16:59:45 +0300

    conflict

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,7 +4,7 @@
 int
 main()
 {
-  std::string name;             // переменная для имени
+  std::string name;             //переменная для имени
   std::getline(std::cin, name); // читаем строку
   std::cout << "Hello World from " << name << std::endl; //выводим результат
   return 0;
